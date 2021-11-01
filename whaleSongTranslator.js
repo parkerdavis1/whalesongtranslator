a2735b26a011f78c016f52d5299ec245419f2361
Parker <parkied@gmail.com>
2021-10-31 21:37:57 -0700

    Fix input display

--- whaleSongTranslator.js
+++ whaleSongTranslator.js
@@ -1,19 +1,20 @@
 let input;
 
 const createWhaleSong = () => {
-  input = document.getElementById('songInput').value.toLowerCase();
+  input = document.getElementById('songInput').value
+  let inputLower = input.toLowerCase();
   const vowels= ['a', 'e', 'i', 'o', 'u'];
   let resultArray = [];
   for (let inputIndex=0; inputIndex<input.length; inputIndex++) {
     for (let vowelsIndex = 0; vowelsIndex<vowels.length; vowelsIndex++) {
-      if (input[inputIndex] === vowels[vowelsIndex]) {
+      if (inputLower[inputIndex] === vowels[vowelsIndex]) {
         resultArray.push(input[inputIndex]);
         
         //below doubles "e"s and "u"s
-        if (input[inputIndex] === 'e') {
+        if (inputLower[inputIndex] === 'e') {
           resultArray.push(input[inputIndex]);
         }
-        if (input[inputIndex] === 'u') {
+        if (inputLower[inputIndex] === 'u') {
           resultArray.push(input[inputIndex]);
         }
       }
