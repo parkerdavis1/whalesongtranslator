6d0c0c5407d80596468bf1beeb9a701e1f7d1015
Parker <parkied@gmail.com>
2021-10-31 21:32:34 -0700

    Minor Formatting

--- whaleSongTranslator.js
+++ whaleSongTranslator.js
@@ -1,22 +1,20 @@
 let input;
 
 const createWhaleSong = () => {
-  input = document.getElementById('songInput').value;
-  let inputLowercase = input.toLowerCase();
+  input = document.getElementById('songInput').value.toLowerCase();
   const vowels= ['a', 'e', 'i', 'o', 'u'];
   let resultArray = [];
-  for (let inputIndex=0; inputIndex<input.length; inputIndex++)
-  {
-    for (let vowelsIndex = 0; vowelsIndex<vowels.length; vowelsIndex++)
-    {
-      if (inputLowercase[inputIndex] === vowels[vowelsIndex])
-      {
-        resultArray.push(inputLowercase[inputIndex]);
-        if (inputLowercase[inputIndex] === 'e') {
-          resultArray.push(inputLowercase[inputIndex]);
+  for (let inputIndex=0; inputIndex<input.length; inputIndex++) {
+    for (let vowelsIndex = 0; vowelsIndex<vowels.length; vowelsIndex++) {
+      if (input[inputIndex] === vowels[vowelsIndex]) {
+        resultArray.push(input[inputIndex]);
+        
+        //below doubles "e"s and "u"s
+        if (input[inputIndex] === 'e') {
+          resultArray.push(input[inputIndex]);
         }
-        if (inputLowercase[inputIndex] === 'u') {
-          resultArray.push(inputLowercase[inputIndex]);
+        if (input[inputIndex] === 'u') {
+          resultArray.push(input[inputIndex]);
         }
       }
     }
